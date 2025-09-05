c120d14e47cc64f97cad6a783b3e94ffcf1d7ceb
Urooj Khan <uroojk844@gmail.com>
2025-09-05 21:40:36 +0530

    [ADD] adding icons

--- app/components/Logo.vue
+++ app/components/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <nuxt-link to="/" class="text-xl">
     <div class="flex gap-2 items-center">
-      <img src="/favicon.svg" class="w-8 block" />
+      <img src="/favicon.svg" class="w-8 hidden md:block" />
       <span>Hirescript</span>
     </div>
   </nuxt-link>
