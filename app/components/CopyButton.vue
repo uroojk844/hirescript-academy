bda979d7b0bda4c5cc8f18dbfc171038f015a2f1
Urooj Khan <uroojk844@gmail.com>
2025-09-28 16:47:01 +0530

    [NEW] Adding tooltip class

--- app/components/CopyButton.vue
+++ app/components/CopyButton.vue
@@ -15,8 +15,9 @@ async function copy() {
 
 <template>
   <button
+    data-tooltip="Copy code"
     @click="copy"
-    class="grid place-items-center hover:opacity-100 active:scale-95 bg-transparent"
+    class="grid place-items-center hover:opacity-100 active:scale-95 bg-transparent tooltip"
   >
     <icon v-if="copied" name="mdi:check" class="text-green-800" />
     <icon v-else name="uil:copy" />
