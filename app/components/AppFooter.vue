59a41816ed01555371e4405f76a640ae5f1caab0
Urooj Khan <uroojk844@gmail.com>
2025-09-29 14:52:30 +0530

    [UPDATE] updating footer title hirecscript academy

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,16 +1,9 @@
-<script setup lang="ts">
-const icons = {
-  'i-mdi-instagram': '',
-  'i-mdi-linkedin': '',
-}
-</script>
-
 <template>
   <footer class="w-full bg-white dark:bg-[#030712] text-gray-600 dark:text-gray-300 border-t border-gray-200 dark:border-gray-700">
     <div class="max-w-8xl mx-auto flex flex-col sm:flex-row justify-between items-start md:items-center gap-12 py-16 px-6 md:px-16">
       <div class="space-y-4 w-full">
         <img src="/favicon.svg" class="not-dark:invert"/>
-        <h1 class="text-slate-950 dark:text-white font-bold text-lg">HireScript</h1>
+        <h1 class="text-slate-950 dark:text-white font-bold text-lg">Hirescript Academy</h1>
         <p class="text-sm">All rights reserved</p>
         <div class="flex space-x-4 mt-2">
           <a href="https://www.instagram.com/hellohirescript/" target="_blank" rel="noopener noreferrer">
